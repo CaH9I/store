5ed4496adc6fa701b60025a3fc516f02bc7d4450
CaH9I <unlimmitedd@gmail.com>
2016-12-26 18:03:12 +0300

    added data for testing

--- store-core/src/com/expertsoft/core/sql/data.sql
+++ store-core/src/com/expertsoft/core/sql/data.sql
@@ -12,3 +12,17 @@ INSERT INTO authorities(username, authority) VALUES
     ('admin', 'ROLE_ADMIN'),
     ('admin', 'ROLE_USER'),
     ('user', 'ROLE_USER');
+    
+INSERT INTO store_order(first_name, last_name, address, phone, delivery_amount, additional_info) VALUES
+    ('James', 'Smith', '135 Evergreen Ave. Wake Forest, NC 27587', '202-555-0118', 10.0, 'Do not delay delivery please'),
+    ('John', 'Johnson', '7716 Wagon St. Kingsport, TN 37660', '202-555-0137', 15.0, 'I''d like you to deliver till December 24');
+INSERT INTO store_order(first_name, last_name, address, phone, delivery_amount) VALUES
+    ('Robert', 'Williams', '8668 East Snake Hill St. Yuba City, CA 95993', '+1-202-555-0106', 5.0);
+    
+INSERT INTO commerce_item(order_id, product_id, price, quantity) VALUES
+    (1, 1, 400.0, 2),
+    (1, 2, 500.0, 3),
+    (1, 3, 550.0, 1),
+    (2, 2, 550.0, 4),
+    (2, 3, 600.0, 2),
+    (3, 3, 500.0, 8);
