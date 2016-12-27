ba9ef7e6b45869a6bd85340dd7a1021eb7a8fe6c
CaH9I <unlimmitedd@gmail.com>
2016-12-27 12:35:52 +0300

    transaction management for integration tests

--- store-core/test/com/expertsoft/core/configuration/TestDataSourceConfig.java
+++ store-core/test/com/expertsoft/core/configuration/TestDataSourceConfig.java
@@ -5,9 +5,13 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.support.ResourceTransactionManager;
 
 @Configuration
+@EnableTransactionManagement
 public class TestDataSourceConfig {
 
     @Bean
@@ -21,6 +25,11 @@ public class TestDataSourceConfig {
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public ResourceTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 
 }
