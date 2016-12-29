1a005f393ac49fee36b5f75f32819cf5b62f30c1
CaH9I <unlimmitedd@gmail.com>
2016-12-29 18:40:43 +0300

    made configuration via XML

--- store-core/src/com/expertsoft/core/configuration/SecurityInitializer.java
+++
@@ -1,5 +0,0 @@
-package com.expertsoft.core.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {}
