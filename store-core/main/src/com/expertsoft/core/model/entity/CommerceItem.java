b85dbe69ef1bd03859967d8ce643cc314360f92a
CaH9I <unlimmitedd@gmail.com>
2017-01-09 17:28:07 +0300

    added test for createOrder()

--- store-core/main/src/com/expertsoft/core/model/entity/CommerceItem.java
+++ store-core/main/src/com/expertsoft/core/model/entity/CommerceItem.java
@@ -1,5 +1,7 @@
 package com.expertsoft.core.model.entity;
 
+import java.util.Objects;
+
 public class CommerceItem {
     private MobilePhone phone;
     private double price;
@@ -36,4 +38,28 @@ public class CommerceItem {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+        long temp = Double.doubleToLongBits(price);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + quantity;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj != null) && (getClass() == obj.getClass())) {
+            CommerceItem other = (CommerceItem) obj;
+            return Objects.equals(other.getPhone(), getPhone())
+                && Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price)
+                && quantity == other.quantity;
+        } else {
+            return false;
+        }
+    }
+
 }
--- store-core/test/src/com/expertsoft/core/service/OrderServiceUnitTest.java
+++ store-core/test/src/com/expertsoft/core/service/OrderServiceUnitTest.java
@@ -3,6 +3,11 @@ package com.expertsoft.core.service;
 import static com.expertsoft.core.util.OrderStates.DELIVERED;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +18,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 import com.expertsoft.core.model.DeliveryDao;
 import com.expertsoft.core.model.OrderDao;
 import com.expertsoft.core.model.ProductDao;
+import com.expertsoft.core.model.entity.CommerceItem;
+import com.expertsoft.core.model.entity.MobilePhone;
 import com.expertsoft.core.model.entity.Order;
+import com.expertsoft.core.service.component.ShoppingCart;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OrderServiceUnitTest {
@@ -77,5 +85,37 @@ public class OrderServiceUnitTest {
         assertEquals(address, order.getAddress());
         assertEquals(phoneNumber, order.getPhoneNumber());
         assertEquals(additionalInfo, order.getAdditionalInfo());
+    }
+
+    @Test
+    public void createOrder() {
+        ShoppingCart cart = new ShoppingCart();
+        Map<Long, Integer> cartItems = cart.getItems();
+        cartItems.put(1L, 1);
+        cartItems.put(2L, 2);
+        cartItems.put(3L, 3);
+
+        List<MobilePhone> phones = new ArrayList<>();
+        MobilePhone phone1 = new MobilePhone(1L, 100);
+        MobilePhone phone2 = new MobilePhone(2L, 200);
+        MobilePhone phone3 = new MobilePhone(3L, 300);
+        phones.add(phone1);
+        phones.add(phone2);
+        phones.add(phone3);
+
+        List<CommerceItem> commerceItems = new ArrayList<>();
+        commerceItems.add(new CommerceItem(phone1, 1, 100));
+        commerceItems.add(new CommerceItem(phone2, 2, 200));
+        commerceItems.add(new CommerceItem(phone3, 3, 300));
+
+        when(productDao.findByIds(cartItems.keySet())).thenReturn(phones);
+        when(deliveryDao.findFixedDeliveryAmount()).thenReturn(5.0);
+
+        Order order = orderService.createOrder(cart);
+
+        assertEquals(1400, order.getSubtotal(), 0);
+        assertEquals(5, order.getDelivery(), 0);
+        assertEquals(1405, order.getTotal(), 0);
+        assertEquals(commerceItems, order.getCommerceItems());
     }
 }
