c8968d597bb9856ec9303c1f0f8f1275283e99f6
Alex Bondarev <alexbondarev@ubuntu-B85M-D2V>
2018-03-14 14:42:46 +0300

    refactoring (used Optional instead of null checking)

--- store-core/src/main/java/com/expertsoft/core/service/ProductService.java
+++ store-core/src/main/java/com/expertsoft/core/service/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -25,12 +26,7 @@ public class ProductService {
     }
 
     public MobilePhone getById(Long id) {
-        MobilePhone result = productRepository.findOne(id);
-
-        if (result != null) {
-            return result;
-        }
-
-        throw new EntityNotFoundException();
+        return Optional.ofNullable(productRepository.findOne(id))
+                .orElseThrow(EntityNotFoundException::new);
     }
 }
