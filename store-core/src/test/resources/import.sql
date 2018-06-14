d978f28fcb85ab6ba0b6da5e5aee2e608f52cf56
Alex Bondarev <alexbondarev@Alexs-MacBook-Pro.local>
2018-06-14 17:12:30 +0300

    added more sample data

--- store-core/src/test/resources/import.sql
+++ store-core/src/test/resources/import.sql
@@ -10,15 +10,6 @@ INSERT INTO account_role(account_id, role_id) VALUES (2, 2);
 INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (1, 'Samsung galaxy SII', '4''''', '14mm', '56mm', 'black', 600, '12MP');
 INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (2, 'Samsung galaxy SIII', '5''''', '16mm', '60mm', 'black', 650, '13MP');
 INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (3, 'Samsung galaxy S6', '5.5''''', '17mm', '62mm', 'black', 750, '16MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (4, 'iPhone 5S', '4.0''''', '12mm', '50mm', 'space gray', 300, '10MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (5, 'iPhone SE', '4.0''''', '12mm', '50mm', 'space gray', 500, '12MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (6, 'iPhone 6S', '4.7''''', '15mm', '58mm', 'space gray', 700, '12MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (7, 'iPhone 6S+', '4.7''''', '15mm', '58mm', 'space gray', 800, '14MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (8, 'iPhone 7', '4.7''''', '15mm', '58mm', 'space gray', 850, '15MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (9, 'iPhone 7+', '4.7''''', '15mm', '58mm', 'space gray', 900, '16MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (10, 'iPhone 8', '5''''', '16mm', '60mm', 'space gray', 950, '17MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (11, 'iPhone 10', '5.2''''', '16mm', '60mm', 'space gray', 1000, '20MP');
-INSERT INTO mobile_phone(id, model, display, length, width, color, price, camera) VALUES (12, 'iPhone X', '5.5''''', '17mm', '62mm', 'metallic', 1100, '25MP');
 
 INSERT INTO store_order(first_name, last_name, address, phone_number, subtotal, delivery, total, additional_info, state, account_id) VALUES ('James', 'Smith', '135 Evergreen Ave. Wake Forest, NC 27587', '202-555-0118', 2850, 10, 2860, 'Do not delay delivery please', 'SUBMITTED', 2);
 INSERT INTO store_order(first_name, last_name, address, phone_number, subtotal, delivery, total, additional_info, state, account_id) VALUES ('John', 'Johnson', '7716 Wagon St. Kingsport, TN 37660', '202-555-0137', 3400, 15, 3415, 'I''d like you to deliver till December 24', 'SUBMITTED', 1);
